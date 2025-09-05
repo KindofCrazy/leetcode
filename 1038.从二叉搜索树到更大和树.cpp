a6450d210a9d40ff14fd557f5627f387a757a8bd
KindofCrazy <221900275@smail.nju.edu.cn>
2025-09-05 15:46:34 +0800

    二叉树-有递有归

---
+++ 1038.从二叉搜索树到更大和树.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=1038 lang=cpp
+ *
+ * [1038] 从二叉搜索树到更大和树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum;
+
+    TreeNode* bstToGst(TreeNode* root) {
+        if (root == nullptr) return nullptr;
+        
+        bstToGst(root->right);
+        sum += root->val;
+        root->val = sum;
+        bstToGst(root->left);
+        return root;
+    }
+};
+// @lc code=end
+
