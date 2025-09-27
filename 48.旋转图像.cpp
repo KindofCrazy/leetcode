d8b388507b7f59495f0011f3bd35624f486aa77d
KindofCrazy <221900275@smail.nju.edu.cn>
2025-09-27 14:43:25 +0800

    hot100-矩阵

--- 48.旋转图像.cpp
+++ 48.旋转图像.cpp
@@ -11,13 +11,10 @@ public:
         int n = matrix.size();
         for (int layer = 0; layer < n/2; layer++) {
             int top = layer, bottom = n-1-layer, left = layer, right = n-1-layer;
-            for (int offset = 0; left+offset < right; offset++) {
-                int temp = matrix[layer][left+offset];
-                
+            for (int offset = 0; left+offset < right; offset++) {                
                 swap(matrix[top][left+offset], matrix[right-offset][left]);
                 swap(matrix[bottom-offset][left], matrix[bottom][right-offset]);
                 swap(matrix[bottom][right-offset], matrix[top+offset][right]);
-                swap(matrix[top+offset][right], temp);
             }
         }
     }
