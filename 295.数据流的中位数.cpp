62fee75e3b9867759a3a72ba6cb748bf8c78b03d
KindofCrazy <221900275@smail.nju.edu.cn>
2025-10-03 12:12:20 +0800

    hot100-堆

--- 295.数据流的中位数.cpp
+++ 295.数据流的中位数.cpp
@@ -7,16 +7,36 @@
 // @lc code=start
 class MedianFinder {
 public:
+    priority_queue<int, vector<int>, less<int>> pqMax;
+    priority_queue<int, vector<int>, greater<int>> pqMin;
+
+
     MedianFinder() {
         
     }
     
     void addNum(int num) {
-        
+        if (pqMax.empty() || num <= pqMax.top()) {
+            pqMax.push(num);
+            if (pqMax.size() > pqMin.size() + 1) {
+                pqMin.push(pqMax.top());
+                pqMax.pop();
+            }
+        } else {
+            pqMin.push(num);
+            if (pqMin.size() > pqMax.size()) {
+                pqMax.push(pqMin.top());
+                pqMin.pop();
+            }
+        }
     }
     
     double findMedian() {
-        
+        if (pqMax.size() > pqMin.size()) {
+            return pqMax.top();
+        }
+
+        return (pqMax.top() + pqMin.top()) / 2.0;
     }
 };
 
