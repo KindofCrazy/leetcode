b004c6d76575c847eb46edb03e92819c2b12562b
KindofCrazy <221900275@smail.nju.edu.cn>
2025-10-05 16:18:38 +0800

    ho100-动态规划

--- 118.杨辉三角.cpp
+++ 118.杨辉三角.cpp
@@ -9,15 +9,14 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
-        for(int i = 0; i < numRows; i++) {
-            vector<int> layer;
-            layer.push_back(1);
-
+        for (int i = 0; i < numRows; i++) {
+            vector<int> layer(i+1);
+            layer[0] = 1;
             for (int j = 1; j < i; j++) {
-                layer.push_back(ans.back()[j-1] + ans.back()[j]);
+                layer[j] = ans.back()[j-1] + ans.back()[j];
             }
+            layer[i] = 1;
 
-            if (i > 0) layer.push_back(1);
             ans.emplace_back(layer);
         }
 
