1210ae715cc10329cd96f103de5ec1ee0aeead4d
KindofCrazy <221900275@smail.nju.edu.cn>
2025-11-09 13:51:05 +0800

    daily-25/11/9

---
+++ 2169.得到-0-的操作数.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=2169 lang=cpp
+ *
+ * [2169] 得到 0 的操作数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int cnt = 0;
+        while (num1 != 0 && num2 != 0) {
+            if (num1 >= num2) {
+                num1 = num1 - num2;
+            } else {
+                num2 = num2 - num1;
+            }
+            
+            cnt++;
+        }
+
+        return cnt;
+    }
+};
+// @lc code=end
+
