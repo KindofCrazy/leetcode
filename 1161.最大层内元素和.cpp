15e98eef7cfa847388654eadee09b929984d73e3
KindofCrazy <221900275@smail.nju.edu.cn>
2026-01-06 16:12:33 +0800

    daily-26/1/6

--- 1161.最大层内元素和.cpp
+++ 1161.最大层内元素和.cpp
@@ -19,32 +19,34 @@
 class Solution {
 public:
     int maxLevelSum(TreeNode* root) {
-        if (root == nullptr) return {};
-
         queue<TreeNode*> q;
-        int ans;
-        int mxsum = INT_MIN;
-
         q.push(root);
-        int level = 1;
+
+        int maxId = 0, maxSum = INT_MIN;
+        int id = 1;
         while (!q.empty()) {
+            int sz = q.size();
             int sum = 0;
-            int n = q.size();
-            for (int i = 0; i < n; i++) {
-                auto node = q.front();
+
+            while (sz--) {
+                TreeNode* node = q.front();
                 q.pop();
+
                 sum += node->val;
 
                 if (node->left) q.push(node->left);
                 if (node->right) q.push(node->right);
             }
 
-            if (sum > mxsum) ans = level;
-            mxsum = max(sum, mxsum);
-            level++;
+            if (sum > maxSum) {
+                maxId = id;
+                maxSum = sum;
+            }
+
+            id++;
         }
 
-        return ans;
+        return maxId;
     }
 };
 // @lc code=end
